81d612b6d393cc8b8d6d1860511d34d8390dcc50
Dominik Grimm <dogrimm@bs-borgwardt04.ethz.ch>
2016-06-22 14:29:49 +0200

    added names of TO, EO, UO

--- arapheno/phenotypedb/tables.py
+++ arapheno/phenotypedb/tables.py
@@ -6,18 +6,18 @@ from django.utils.safestring import mark_safe
 class PhenotypeTable(tables.Table):
     name = tables.LinkColumn("phenotype_detail",args=[A('id')],text=lambda record: record.name,verbose_name="Phenotype Name",order_by="name")
     study = tables.LinkColumn("study_detail",args=[A('study.id')],text=lambda record: record.study.name,verbose_name="Study",order_by="study.name")
-    to = tables.Column(accessor="to_term",verbose_name="TO",order_by="to_term")
-    eo = tables.Column(accessor="eo_term",verbose_name="EO",order_by="eo_term")
-    uo = tables.Column(accessor="uo_term",verbose_name="UO",order_by="uo_term")
+    to = tables.Column(accessor="to_term",verbose_name="Trait Ontology (TO)",order_by="to_term")
+    eo = tables.Column(accessor="eo_term",verbose_name="Environmental Ontoloy (EO)",order_by="eo_term")
+    uo = tables.Column(accessor="uo_term",verbose_name="Unit Ontology (UO)",order_by="uo_term")
     
     class Meta:
         attrs = {"class": "striped"}
 
 class ReducedPhenotypeTable(tables.Table):
     name = tables.LinkColumn("phenotype_detail",args=[A('id')],text=lambda record: record.name,verbose_name="Phenotype Name",order_by="name")
-    to = tables.Column(accessor="to_term",verbose_name="TO",order_by="to_term")
-    eo = tables.Column(accessor="eo_term",verbose_name="EO",order_by="eo_term")
-    uo = tables.Column(accessor="uo_term",verbose_name="UO",order_by="uo_term")
+    to = tables.Column(accessor="to_term",verbose_name="Trait Ontology (TO)",order_by="to_term")
+    eo = tables.Column(accessor="eo_term",verbose_name="Environmental Ontoloy (EO)",order_by="eo_term")
+    uo = tables.Column(accessor="uo_term",verbose_name="Unit Ontology (UO)",order_by="uo_term")
     
     class Meta:
         attrs = {"class": "striped"}
