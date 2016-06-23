ac1b483acfb005e458009684d04df141069d6ae9
Dominik Grimm <dogrimm@bs-borgwardt04.ethz.ch>
2016-06-23 18:03:20 +0200

    fixed some small bugs

--- arapheno/arapheno/urls.py
+++ arapheno/arapheno/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     url(r'phenotypes/$',phenotypedb.views.PhenotypeList,name="phenotypes"),
     url(r'phenotype/(?P<pk>[0-9]+)/$',phenotypedb.views.PhenotypeDetail.as_view(),name="phenotype_detail"),
     url(r'studies/$',phenotypedb.views.StudyList,name="studies"),
-    url(r'study/(?P<pk>[0-9]+)$',phenotypedb.views.StudyDetail,name="study_detail"),
+    url(r'study/(?P<pk>[0-9]+)/$',phenotypedb.views.StudyDetail,name="study_detail"),
     url(r'about/$',home.views.about),
     url(r'faq/$',home.views.faq),
     url(r'faq/content/$',home.views.faqcontent),
