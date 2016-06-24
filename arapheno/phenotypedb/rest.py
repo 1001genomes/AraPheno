fd7b6a923013baa12a8914de5b95b06e9a59edfd
Dominik Grimm <dogrimm@bs-borgwardt04.ethz.ch>
2016-06-24 14:14:05 +0200

    cleaned up

--- arapheno/phenotypedb/rest.py
+++ arapheno/phenotypedb/rest.py
@@ -1,5 +1,4 @@
 from django.http import HttpResponse
-#from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view, permission_classes, renderer_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
