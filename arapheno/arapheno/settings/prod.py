7ad8c763799c3b142e606bdd095be8fc561d1481
Uemit Seren <uemit.seren@gmail.com>
2016-08-14 17:11:46 +0200

    Added Email settings

--- arapheno/arapheno/settings/prod.py
+++ arapheno/arapheno/settings/prod.py
@@ -3,7 +3,7 @@ Production settings
 """
 # Load defaults in order to then add/override with dev-only settings
 from defaults import *
-
+import os
 
 DATACITE_REST_URL='https://mds.datacite.org/'
 
@@ -14,3 +14,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+EMAIL_HOST = os.eniron["EMAIL_HOST"]
+EMAIL_PORT = os.environ.get("EMAIL_PORT",25)
+EMAIL_HOST_USER = os.environ["EMAIL_USER"]
