8e6016a6237e179d7c36a6aa3eeff08a08e726e0
Ümit Seren <timeu@users.noreply.github.com>
2020-08-18 10:42:59 +0200

    Increase timeout for gunicorn to 300
    This should fix 405 errors when uploading bug studies

--- web-entrypoint.sh
+++ web-entrypoint.sh
@@ -5,7 +5,7 @@ if [ "$1" = 'manage.py' ]; then
   echo "Starting server..."
   ./manage.py migrate
   ./manage.py collectstatic --noinput
-  exec /usr/local/bin/gunicorn arapheno.wsgi:application -w 2 -b :8000
+  exec /usr/local/bin/gunicorn arapheno.wsgi:application -w 2 --timeout 300 -b :8000
 fi
 echo "Runing command..."
 exec "$@"
