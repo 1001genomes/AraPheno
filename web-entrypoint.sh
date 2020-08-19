35e5ede0b23ac83f7757b351bfef83243200bc8f
Ümit Seren <timeu@users.noreply.github.com>
2020-08-19 19:15:38 +0200

    Increase worker number and restart after 1000 requests
    This should workaround memory leaks

--- web-entrypoint.sh
+++ web-entrypoint.sh
@@ -5,7 +5,7 @@ if [ "$1" = 'manage.py' ]; then
   echo "Starting server..."
   ./manage.py migrate
   ./manage.py collectstatic --noinput
-  exec /usr/local/bin/gunicorn arapheno.wsgi:application -w 2 --timeout 300 -b :8000
+  exec /usr/local/bin/gunicorn arapheno.wsgi:application -w 9 --max-requests 1000 --max-requests-jitter 50 --timeout 300 -b :8000
 fi
 echo "Runing command..."
 exec "$@"
