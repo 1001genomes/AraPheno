6f873e4d79a3b72f3cfc1473c17ea4710d7ac9c7
Ümit Seren <uemit.seren@gmi.oeaw.ac.at>
2023-01-31 16:31:53 +0100

    Remove Admins

--- arapheno/arapheno/settings/defaults.py
+++ arapheno/arapheno/settings/defaults.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__+ "/../")))
 
-ADMINS = [('Uemit', 'uemit.seren@gmi.oeaw.ac.at')]
+ADMINS = []
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
